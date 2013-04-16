c02266dc2725f8d03a206cc8e41087edae7961a3
ubundude <golfguy90@gmail.com>
2013-04-16 15:07:53 -0500

    Did an sql call, I think

--- grails-app/controllers/school/GradeViewController.groovy
+++ grails-app/controllers/school/GradeViewController.groovy
@@ -1,8 +1,13 @@
 package school
 
+import groovy.sql.Sql
+
 class GradeViewController {
 
     def index() {
+        sql = Sql.newInstance( 'jdbc:jtds:sqlserver://localhost/dbName-elearn','kolby',
+                'Cheese85', 'net.sourceforge.jtds.jdbc.Driver')
 
+        sql.rowEach('select ')
     }
 }
