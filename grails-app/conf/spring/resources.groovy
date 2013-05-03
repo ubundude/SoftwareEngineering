1cf323bf045d91d5b92b59dae295bd5718dfcf83
ubundude <golfguy90@gmail.com>
2013-05-03 09:56:26 -0500

    other thing

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -2,14 +2,14 @@ import org.codehaus.groovy.grails.plugins.springsecurity.SpringSecurityUtils
 
 // Place your Spring DSL code here
 beans = {
-//    authenticationSuccessHandler(elearn.MyAuthSuccessHandler) {
-//        def conf = SpringSecurityUtils.securityConfig
-//        requestCache = ref('requestCache')
-//        defaultTargetUrl = conf.successHandler.defaultTargetUrl
-//        alwaysUseDefaultTargetUrl = conf.successHandler.alwaysUseDefault
-//        targetUrlParameter = conf.successHandler.targetUrlParameter
-//        useReferer = conf.successHandler.useReferer
-//        redirectStrategy = ref('redirectStrategy')
-//        //goToUrl = "userHome/index"
-//    }
+    authenticationSuccessHandler(elearn.MyAuthSuccessHandler) {
+        def conf = SpringSecurityUtils.securityConfig
+        requestCache = ref('requestCache')
+        defaultTargetUrl = conf.successHandler.defaultTargetUrl
+        alwaysUseDefaultTargetUrl = conf.successHandler.alwaysUseDefault
+        targetUrlParameter = conf.successHandler.targetUrlParameter
+        useReferer = conf.successHandler.useReferer
+        redirectStrategy = ref('redirectStrategy')
+        goToUrl = "userHome/index"
+    }
 }
